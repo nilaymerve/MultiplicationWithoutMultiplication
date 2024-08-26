702ba61dfc3be98376a1bc7b9d6f31b9e7c8fe64
Hp <Hp@MERVESU>
2024-08-26 14:54:09 +0300

    Add project files.

---
+++ Algoritma30122023/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritma30122023
+{
+    /* kullanıcıdan pozitif olan max 2 basamaklı bir sayı alınacaktır. Alınan bu sayının okunuşu yazı ile yazılacaktır.
+  * 11 onbir
+  */
+  
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = 2, s2 = 3, sonuc = 0;
+            for(int i = 0; i < s2; i++)
+            {
+                sonuc += s1;
+            }
+            Console.WriteLine(sonuc);
+            Console.ReadKey();
+        } 
+    }
+}
